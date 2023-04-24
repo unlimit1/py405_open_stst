4f64279ae13b3b1cb14b79f1bd0e86674f07928c
l <unlimit@gmail.com>
2023-04-25 02:35:46 +0900

    docker 환경구성중2

--- py/test_naver_daily_ohlc.py
+++ py/test_naver_daily_ohlc.py
@@ -13,8 +13,9 @@ pages = 10
 # Define empty list to store data
 data = []
 
-
-for page_num in range(1, num_pages+1):
+i=0
+for page_num in range(1, pages+1):
+    print(f"{i} ", end='')
     # 현재 페이지의 HTML 코드 가져오기
     response = requests.get(url+str(page_num))
     # BeautifulSoup 객체 생성
@@ -23,15 +24,16 @@ for page_num in range(1, num_pages+1):
     table = soup.find_all('table')[1]
     # pandas의 read_html 함수를 사용하여 테이블 태그를 데이터프레임으로 변환
     df = pd.read_html(str(table))[0]
+    df.dropna()
     # 필요한 열만 선택
-    df = df[['종목명', '현재가', '전일비', '등락률', '시가', '고가', '저가', '거래량']]
+    #df = df[['종목명', '현재가', '전일비', '등락률', '시가', '고가', '저가', '거래량']]
+    #df = df[['N', '종목명', '현재가', '전일비', '등락률', '액면가', '시가총액', '상장주식수', '외국인비율', '거래량', 'PER', 'ROE', '토론실']]
     # 열 이름 변경
-    df.columns = ['Name', 'Price', 'Diff',
-                  'Change', 'Open', 'High', 'Low', 'Volume']
+    # df.columns = ['Name', 'Price', 'Diff', 'Change', 'Open', 'High', 'Low', 'Volume']
     # 데이터프레임을 리스트에 추가
     data.append(df)
     # 1초 대기
-    time.sleep(1)
+    #time.sleep(0.1)
 
 # Concatenate all data into one dataframe
 result = pd.concat(data)
