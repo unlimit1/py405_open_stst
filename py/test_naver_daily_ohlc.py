7ede139ccc4fbeea9b288a45c4ebb91e8fae19cd
l <unlimit@gmail.com>
2023-04-26 17:48:47 +0900

    작업중

--- py/test_naver_daily_ohlc.py
+++ py/test_naver_daily_ohlc.py
@@ -13,31 +13,27 @@ pages = 10
 # Define empty list to store data
 data = []
 
-i=0
 for page_num in range(1, pages+1):
-    print(f"{i} ", end='')
-    # 현재 페이지의 HTML 코드 가져오기
+    print(f"{page_num} ", end='')
     response = requests.get(url+str(page_num))
-    # BeautifulSoup 객체 생성
     soup = BeautifulSoup(response.content, 'html.parser')
-    # HTML 코드에서 두 번째 테이블 태그 가져오기
     table = soup.find_all('table')[1]
-    # pandas의 read_html 함수를 사용하여 테이블 태그를 데이터프레임으로 변환
-    df = pd.read_html(str(table))[0]
-    df.dropna()
-    # 필요한 열만 선택
-    #df = df[['종목명', '현재가', '전일비', '등락률', '시가', '고가', '저가', '거래량']]
-    #df = df[['N', '종목명', '현재가', '전일비', '등락률', '액면가', '시가총액', '상장주식수', '외국인비율', '거래량', 'PER', 'ROE', '토론실']]
-    # 열 이름 변경
-    # df.columns = ['Name', 'Price', 'Diff', 'Change', 'Open', 'High', 'Low', 'Volume']
-    # 데이터프레임을 리스트에 추가
+    df = pd.read_html(str(table), encoding='utf-8')[0]
+    # 결측값이 있는 행 제거
+    df = df.dropna(how='all')
+    # 'N' 열의 데이터 타입을 정수형으로 변환
+    df['N'] = df['N'].astype(int)
+    # 현재 페이지 번호 출력
+    print(f"{page_num} ", end='')
+    # 변환된 데이터프레임을 리스트에 추가
     data.append(df)
-    # 1초 대기
-    #time.sleep(0.1)
 
 # Concatenate all data into one dataframe
 result = pd.concat(data)
 
 # Save the result as csv file
-result.to_csv('naver_stock.csv', index=False)
+file_name = 'naver_stock.csv'
+result.to_csv(file_name, index=False)
 
+with open(file_name, 'r') as f: data = f.read()
+with open(file_name, 'w') as f: f.write('현재 시각 : ' + time.strftime('%Y-%m-%d %H:%M:%S') + '\n' + data)
